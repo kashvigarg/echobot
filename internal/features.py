f88a7aaf3430310411982c537530261b42bbca97
Dhruv Jain <92215138+jaydee029@users.noreply.github.com>
2024-02-23 08:46:21 +0000

    refactored

--- internal/features.py
+++ internal/features.py
@@ -2,26 +2,26 @@ import whisper
 from jsonf import *
 
 
-def detect_lang(model,audio):
+async def detect_lang(model,audio):
     audio_test = whisper.pad_or_trim(audio) #fits the audio into 30s 
     test=model.transcribe(audio_test)
     lang=test["language"]
 
-    if lang not in ['id','vi','th','my','ms','zh']:
+    if lang not in ['id','vi','th','my','ms','zh',]:
         return False
     
-def trans_eng(audio):
+async def trans_eng(audio):
     model=whisper.load_model("small")
-    if detect_lang(model,audio)==False:
+    if await detect_lang(model,audio)==False:
         return False
     eng=model.transcribe(audio,task="translate")
     eng_json(eng["text"])
 
 
-def trans_both(audio):
+async def trans_both(audio):
     model=whisper.load_model("small")
     
-    if detect_lang(model,audio)==False:
+    if await detect_lang(model,audio)==False:
         return False
     
     sea_lang=model.transcribe(audio)
